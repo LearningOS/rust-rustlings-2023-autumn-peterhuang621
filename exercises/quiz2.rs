d528e684f69275fb517cce71b41b3dc22334a8b7
黄熠 <peterhuang@ehuang98-Mac-Air.local>
2023-11-13 20:13:29 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    Committer: 黄熠 <peterhuang@ehuang98-Mac-Air.local>
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
          modified:   exercises/clippy/clippy1.rs
          modified:   exercises/clippy/clippy2.rs
          modified:   exercises/clippy/clippy3.rs
          modified:   exercises/enums/enums1.rs
          modified:   exercises/enums/enums2.rs
          modified:   exercises/enums/enums3.rs
          modified:   exercises/error_handling/errors1.rs
          modified:   exercises/error_handling/errors2.rs
          modified:   exercises/error_handling/errors3.rs
          modified:   exercises/error_handling/errors4.rs
          modified:   exercises/error_handling/errors5.rs
          modified:   exercises/error_handling/errors6.rs
          modified:   exercises/generics/generics1.rs
          modified:   exercises/generics/generics2.rs
          modified:   exercises/hashmaps/hashmaps1.rs
          modified:   exercises/hashmaps/hashmaps2.rs
          modified:   exercises/hashmaps/hashmaps3.rs
          modified:   exercises/iterators/iterators1.rs
          modified:   exercises/iterators/iterators2.rs
          modified:   exercises/lifetimes/lifetimes1.rs
          modified:   exercises/lifetimes/lifetimes2.rs
          modified:   exercises/lifetimes/lifetimes3.rs
          modified:   exercises/modules/modules1.rs
          modified:   exercises/modules/modules2.rs
          modified:   exercises/modules/modules3.rs
          modified:   exercises/move_semantics/move_semantics3.rs
          modified:   exercises/move_semantics/move_semantics4.rs
          modified:   exercises/options/options1.rs
          modified:   exercises/options/options2.rs
          modified:   exercises/options/options3.rs
          modified:   exercises/quiz2.rs
          modified:   exercises/quiz3.rs
          modified:   exercises/strings/strings1.rs
          modified:   exercises/strings/strings2.rs
          modified:   exercises/strings/strings3.rs
          modified:   exercises/strings/strings4.rs
          modified:   exercises/structs/structs1.rs
          modified:   exercises/structs/structs2.rs
          modified:   exercises/structs/structs3.rs
          modified:   exercises/tests/tests1.rs
          modified:   exercises/tests/tests2.rs
          modified:   exercises/tests/tests3.rs
          modified:   exercises/tests/tests4.rs
          modified:   exercises/traits/traits1.rs
          modified:   exercises/traits/traits2.rs
          modified:   exercises/traits/traits3.rs
          modified:   exercises/traits/traits4.rs
          modified:   exercises/traits/traits5.rs

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,18 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(string: &String,command:&String) -> Vector(String) {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: String = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let mut base = string.clone().to_string();
+            let putin = match command {
+                Command::Append(sz) => base + "bar".repeat(*sz).as_str(),
+                Command::Trim => base.trim().to_string(),
+                Command::Uppercase => base.to_uppercase(),
+            };
+            output.push(putin)
         }
         output
     }
@@ -45,12 +50,12 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
+        let output: Vec<String> = transformer(vec![
             ("hello".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
